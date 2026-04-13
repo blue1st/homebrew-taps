b1cfb39aafd7247176915be4a169b1a220771392
Developer <developer@example.com>
2026-04-13 10:29:59 +0900

    chore: update URL template to use version variable for caffei-native cask

--- Casks/caffei-native.rb
+++ Casks/caffei-native.rb
@@ -2,7 +2,7 @@ cask "caffei-native" do
   version "0.1.8"
   sha256 "3adbe0e8cf3e7975791da7bede9cae8eeac201d73e213582724185902e71e6a1"
 
-  url "https://github.com/blue1st/caffei-native/releases/download/v#{version}/Caffei Native_0.1.8_aarch64.dmg"
+  url "https://github.com/blue1st/caffei-native/releases/download/v#{version}/Caffei%20Native_#{version}_aarch64.dmg"
   name "Caffei Native"
   desc "Sleep suppression tool with process monitoring"
   homepage "https://github.com/blue1st/caffei-native"
