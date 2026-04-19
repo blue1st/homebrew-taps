090b23b044db39a51888d04a859e5ee2845db375
Developer <developer@example.com>
2026-04-19 16:01:30 +0900

    feat: add cask for danmaku-electron version 1.2.4

---
+++ Casks/danmaku-electron.rb
@@ -0,0 +1,17 @@
+cask "danmaku-electron" do
+  version "1.2.4"
+  sha256 "6388f047f22282e9e0e75635c79a67f1937ae70e19fc95dea2c9c39cc2f5d745"
+
+  url "https://github.com/blue1st/danmaku-electron/releases/download/v#{version}/danmaku-electron-#{version}-arm64.dmg"
+  name "Danmaku Electron"
+  desc "AI Desktop Commentary Overlay"
+  homepage "https://github.com/blue1st/danmaku-electron"
+
+  app "danmaku-electron.app"
+
+  zap trash: [
+    "~/Library/Application Support/danmaku-electron",
+    "~/Library/Preferences/com.example.danmaku.plist",
+    "~/Library/Logs/danmaku-electron",
+  ]
+end
