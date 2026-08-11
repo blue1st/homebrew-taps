04e9ff1aeb9b649cee881cff8c127883e2ef60c4
Developer <developer@example.com>
2026-08-12 00:29:35 +0900

    fix(cask): update repository URL to blue1st/NapeProHelper for napepro-helper

--- Casks/napepro-helper.rb
+++ Casks/napepro-helper.rb
@@ -2,10 +2,10 @@ cask "napepro-helper" do
   version "1.0.2"
   sha256 "914dbd5ea59d5a131706ba332e2574e26e9627a78ac3aef931e4e1e02c877078"
 
-  url "https://github.com/blue1st/napepro-helper/releases/download/v#{version}/Nape.Pro.Helper_1.0.2_universal.dmg"
+  url "https://github.com/blue1st/NapeProHelper/releases/download/v#{version}/Nape.Pro.Helper_#{version}_universal.dmg"
   name "Nape Pro Helper"
   desc "System tray companion helper application for Keychron Nape Pro trackball device"
-  homepage "https://github.com/blue1st/napepro-helper"
+  homepage "https://github.com/blue1st/NapeProHelper"
 
   app "Nape Pro Helper.app"
 
